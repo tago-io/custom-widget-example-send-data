322d44034d8a19a4f719b8d372885e3d18b1991e
Gustavo Bonassa <gustavo.bonassa1@gmail.com>
2020-04-21 15:18:52 -0300

    feat: basic send data example ready

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -5,28 +5,12 @@ import "@tago/custom-widget/dist/custom-widget.css"
 import "./Widget.css";
 
 function Widget() {
-  const [widgetTitle, setWidgetTitle] = useState("");
-  const [variable, setVariable] = useState({});
-  // input text
-  const [text, setText] = useState("");
+  const [varName, setVarName] = useState("");
+  const [value, setValue] = useState("");
   const [response, setResponse] = useState("");
 
   useEffect(() => {
-    window.TagoIO.onStart(null, (widget) => {
-      setWidgetTitle(widget.label);
-      // get the name of the first variable
-      setVariable({ variable: widget.display.variables[0].variable });
-    });
-
-    window.TagoIO.onRealtime((data) => {
-      // get the last variable from realtime
-      if (data && data.result.length) {
-        setVariable({
-          variable: data.result[0].variable,
-          value: data.result[0].value,
-        })
-      }
-    })
+    window.TagoIO.onStart();
   }, []);
 
   // clears the return message when sending data
@@ -38,8 +22,8 @@ function Widget() {
 
   const sendData = () => {
     const payload = [{ 
-      ...variable,
-      value: text 
+      variable: varName,
+      value
     }];
     const options = {
       // complete the variables with the bucket and origin ids (if this option is false, this information needs to be passed)
@@ -61,12 +45,8 @@ function Widget() {
   return (
     <div className="container">
       <div>
-        <h1>{widgetTitle}</h1>
-        <h2>Variable: {variable.variable}</h2>
-        <h2>Value: {variable.value}</h2>
-      </div>
-      <div>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+        <input type="text" value={varName} onChange={(e) => setVarName(e.target.value)}/>
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
         <button onClick={sendData}>Send data</button>
         {response && (
           <div className="alert">{response}</div>
